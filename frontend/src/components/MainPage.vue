5c4dbda6d150ef1d3a1df6c4af6625272638ea8f
awei-mj <2510558223@qq.com>
2024-03-31 19:54:24 +0800

    自制图片上传组件 完成数据绑定

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -1,13 +1,14 @@
-<script setup lang="ts">
-  import { ref } from 'vue'
+<script lang="ts" setup>
+  import { ref, watch, Ref } from 'vue';
   import { Delete, ZoomIn, UploadFilled } from '@element-plus/icons-vue'
 
-  import type { UploadFile } from 'element-plus'
+  import type { UploadFile, UploadUserFile } from 'element-plus'
 
   const dialogImageUrl = ref('')
   const dialogVisible = ref(false)
-  const disabled = ref(false)
-  const fileList = ref()
+  //const disabled = ref(false)
+  const files : Ref<FileList | null> = ref(null)
+  const fileList = ref(new Array<UploadUserFile>)
   const handlePictureCardPreview = (file: UploadFile) => {
     dialogImageUrl.value = file.url!
     dialogVisible.value = true
@@ -16,20 +17,68 @@
     let index = fileList.value.indexOf(file);
     fileList.value.splice(index, 1);
   }
+  const getFileSize = (file: UploadFile) => {
+    if(file.size !== undefined) {
+      if(file.size < 1024)
+        return file.size.toFixed(2) + 'B';
+      else if(file.size < 1048576)
+        return (file.size / 1024).toFixed(2) + 'KiB';
+      else
+        return (file.size / 1048576).toFixed(2) + 'MiB';
+    }
+    return undefined;
+  }
+  const handleChange = () => {
+    const input = document.getElementById("input") as HTMLInputElement;
+    files.value = input.files!;
+    fileList.value.splice(0, fileList.value.length);
+    for (let i = 0; i < files.value.length; i++) {
+      let file = files.value[i];
+      fileList.value.push({
+        name: file.name,
+        size: file.size,
+        status: 'ready',
+        uid: i,
+        url: URL.createObjectURL(file)
+      })
+    }
+  }
+  watch(files, (newList) => {
+    (document.getElementById("input") as HTMLInputElement).files = newList
+    fileList.value.splice(0, fileList.value.length);
+    for (let i = 0; i < files.value!.length; i++) {
+      let file = files.value![i];
+      fileList.value.push({
+        name: file.name,
+        size: file.size,
+        status: 'ready',
+        uid: i,
+        url: URL.createObjectURL(file)
+      })
+    }
+  })
 </script>
 
 <template>
-  <el-upload drag multiple action="#" list-type="picture-card" v-model:file-list="fileList" :auto-upload="false">
+  <input multiple type="file" accept="image/*" id="input" @change="handleChange"/>
+
+  <ul>
+    <li v-for="file in fileList">
+      <img :src="file.url" :alt="file.name" />
+    </li>
+  </ul>
+
+  <!-- <el-upload drag multiple action="#" list-type="picture" v-model:file-list="fileList" :auto-upload="false">
     <el-icon class="el-icon--upload"><upload-filled /></el-icon>
     <div class="el-upload__text">
       Drop file here or <em>click to upload</em>
     </div>
 
     <template #file="{ file }">
-      <div>
+      <div style="width: 500px;">
         <img class="el-upload-list__item-thumbnail" :src="file.url" alt="" />
-        <span>{{ file.size }}</span>
-        <span>{{ file.name }}</span>
+        <span class="el-upload-list__item-thumbnail">{{ getFileSize(file) }}</span>
+        <span class="el-upload-list__item-thumbnail">{{ file.name }}</span>
         <span class="el-upload-list__item-actions">
           <span
             class="el-upload-list__item-preview"
@@ -39,7 +88,7 @@
           </span>
           <span
             v-if="!disabled"
-            class="el-upload-list__item-delete"
+            class="el-upload-list__item-preview"
             @click="handleRemove(file)"
           >
             <el-icon><Delete /></el-icon>
@@ -47,8 +96,9 @@
         </span>
       </div>
     </template>
+  </el-upload> -->
 
-  </el-upload>
+  <el-button>上传图片</el-button>
 
   <el-dialog v-model="dialogVisible">
     <img w-full :src="dialogImageUrl" alt="Preview Image" />
